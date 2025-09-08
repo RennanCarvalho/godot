b12e21dcfd79d65a07929450d690c15110931930
rennan.carvalho <rennan@cbpm.sp.gov.br>
2025-09-08 13:59:23 -0300

    double jump logic

--- first/scripts/player.gd
+++ first/scripts/player.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+var canDoubleJump = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+ 	# Gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Pulo
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			canDoubleJump = true  # reseta double jump ao tocar o chão
+		elif canDoubleJump:
+			velocity.y = JUMP_VELOCITY
+			canDoubleJump = false  # usa o double jump
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimento horizontal
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
