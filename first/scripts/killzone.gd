83bf24f7df7e322c93402487919446f69604dd86
rennan.carvalho <rennan@cbpm.sp.gov.br>
2025-09-17 09:18:34 -0300

    first game complete

--- first/scripts/killzone.gd
+++ first/scripts/killzone.gd
@@ -3,4 +3,5 @@ extends Area2D
 @onready var slime: Node2D = $".."
 
 func _on_body_entered(_body: Node2D) -> void:
-	slime.punch()
+	if get_parent().is_in_group("enemies"): slime.punch()
+	else: get_tree().reload_current_scene()
