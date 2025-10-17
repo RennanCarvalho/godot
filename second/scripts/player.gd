fa48e4311e6bed802861b262f11d10c4fd05af92
rennan.carvalho <rennan@cbpm.sp.gov.br>
2025-10-17 14:31:02 -0300

    adjust physics

--- second/scripts/player.gd
+++ second/scripts/player.gd
@@ -4,8 +4,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
